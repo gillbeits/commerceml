de5a5fab9bb31fd5fe2cd0cef9cffbb91889df6e
Ivan Koretskiy <gillbeits@gmail.com>
2015-04-17 18:52:22 +0300

    Init commit

---
+++ src/CommerceMLParser/Parser.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ivan Koretskiy aka gillbeits[at]gmail.com
+ * Date: 17/04/15
+ * Time: 19:47
+ */
+
+namespace CommerceMLParser;
+
+
+class Parser {
+
+}
